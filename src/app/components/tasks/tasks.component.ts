884f62ae0315b5e0688df0f02a2e530507825445
LucianoASanto <lucho.ss@outlook.com>
2023-02-26 10:27:14 -0300

    componente lista de items

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { TaskService } from 'src/app/service/task.service';
 import { Task } from 'src/app/Task';
-import { TASKS } from 'src/app/mock-task';
+
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent {
-  tasks : Task[] = TASKS;
+  tasks : Task[] = [];
+
+  constructor(
+    private TaskService: TaskService
+      
+    
+  ){ }
+
+  ngOnInit(): void{
+    this.TaskService.getTask().subscribe((tasks)=>[
+      this.tasks = tasks
+    ]);
+    
+  }
 }
